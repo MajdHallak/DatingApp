c9320556b8d4ff5fb3ac98935e7e3dd4a1ee4138
majdhallak <majdhallak90@gmail.com>
2024-08-28 01:58:43 +0100

    input & output - 56.

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  private accountService = inject(AccountService);
+  // @Input() usersFromHomeComponent: any;
+  // @Output() cancelRegister = new EventEmitter();
+  //                             |
+  // introduced in Angular 17.2 \|/
+  usersFromHomeComponent = input.required<any>();
+  cancelRegister = output<boolean>();
+
+  model: any = {};
+
+  register() {
+    console.log(this.model);
+    this.accountService.register(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.cancel();
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+}
